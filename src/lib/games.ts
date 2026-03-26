85e1c16b339e6141a87c37ba2d248c04b2f10e60
NEKOmatta <nekomatta77@gmail.com>
2026-03-26 12:57:27 +0800

    3

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -3,14 +3,6 @@ import { Game } from '../types';
 
 export const GAMES: Game[] = [
   {
-    id: 'tictactoe',
-    name: 'Крестики-нолики',
-    description: 'Классическая игра 3x3. Собери 3 в ряд для победы!',
-    image: 'https://picsum.photos/seed/tictactoe/400/300?blur=2',
-    minPlayers: 2,
-    maxPlayers: 8,
-  },
-  {
     id: 'drawphone',
     name: 'Испорченный телефон',
     description: 'Рисуй и угадывай! Безумная цепочка слов и рисунков.',
